9332c922ff4067048cf24c4fdb834da99118a0bb
eto <eto@e04a658f-9c09-0410-9798-f83fe6973026>
2007-06-19 06:51:25 +0000

    update.

--- test/test_sgl.rb
+++ test/test_sgl.rb
@@ -1,11 +1,30 @@
+#!/usr/bin/env ruby -w
+# Copyright (C) 2004-2007 Kouichirou Eto, All rights reserved.
+
 require File.dirname(__FILE__) + '/test_helper.rb'
 
-class TestSgl < Test::Unit::TestCase
+require 'test_helper'
+require 'test-all'
+#require 'test-cocoa-app'
+require 'test-module-ruby16'
+require 'test-opengl-app'
+require 'test-opengl-basic'
+require 'test-opengl-fullscreen'
+require 'test-opengl-novice'
 
-  def setup
-  end
-  
-  def test_truth
-    assert true
-  end
+$LOAD_PATH.unshift("..") if !$LOAD_PATH.include?("..")
+
+$VERBOSE = true
+
+def load_files(dir, base)
+  Dir.chdir(dir)
+  Dir.glob(base+"*.rb") {|filename|
+    require filename
+  }
 end
+
+def main(argv)
+  load_files("..", "sgl/test-opengl-")
+  load_files("..", "sgl/sgl-")
+end
+main(ARGV)
