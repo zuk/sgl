a78805da6fee5acefc58eff0726027b28236bede
eto <eto@e04a658f-9c09-0410-9798-f83fe6973026>
2007-06-20 13:21:27 +0000

    update.

--- website/ex1.rb
+++
@@ -1,6 +0,0 @@
-require "sgl/opengl"  # プログラムの最初に必ず書く必要がある
-window 100, 100       # windowを開く
-background 0          # 背景を黒にする
-color 100             # 描画する色を白にする
-line 0, 0, 100, 100   # 左下から右上に線を描く
-wait                  # そのまま待つ
