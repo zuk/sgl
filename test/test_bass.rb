9ad44e7e966986672d92f24001cd22d2aa7009c6
eto <eto@e04a658f-9c09-0410-9798-f83fe6973026>
2007-06-25 12:04:27 +0000

    update.

--- test/test_bass.rb
+++ test/test_bass.rb
@@ -13,22 +13,16 @@ class TestSample < Test::Unit::TestCase
     samp = Bass::Sample.new(TEST_FILENAME)
 
     # test_play
-    samp.play; sleep 0.1
+    samp.play; sleep 0.05
 
     # test_note
-    [0, 2, 4, 5, 7, 9, 11, 12].each {|n|
-      samp.play(60 + n); sleep 0.05
-    }
+    [0, 2, 4, 5, 7, 9, 11, 12].each {|n| samp.play(60 + n); sleep 0.05 }
 
     # test_volume
-    10.times {|v|
-      samp.play(60, v * 10); sleep 0.05
-    }
+    10.times {|v| samp.play(60, v * 10); sleep 0.05 }
 
     # test_pan
-    10.times {|pos|
-      samp.play(60, 100, pos * 20 - 100); sleep 0.05
-    }
+    10.times {|pos| samp.play(60, 100, pos * 20 - 100); sleep 0.05 }
   end
 
   def nu_test_1_change_freq
